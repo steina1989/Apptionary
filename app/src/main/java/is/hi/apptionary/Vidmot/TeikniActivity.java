441bb32f322603972cf12b7817e80b9d69bea261
Daniel <dbg10@hi.is>
2018-02-27 16:55:59 +0000

    grunnviðmóti bætt við.

--- app/src/main/java/is/hi/apptionary/Vidmot/TeikniActivity.java
+++ app/src/main/java/is/hi/apptionary/Vidmot/TeikniActivity.java
@@ -1,9 +1,11 @@
-package is.hi.apptionary;
+package is.hi.apptionary.Vidmot;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import is.hi.apptionary.R;
 
 public class TeikniActivity extends AppCompatActivity {
     PaintView canvas;
