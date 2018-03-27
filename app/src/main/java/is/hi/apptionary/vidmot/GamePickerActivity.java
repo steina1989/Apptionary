edcdd3fb07143d2804b36e21157a776a88ab913a
Steina <steina.dogg@gmail.com>
2018-03-27 17:03:08 +0000

    Center toast.

--- app/src/main/java/is/hi/apptionary/vidmot/GamePickerActivity.java
+++ app/src/main/java/is/hi/apptionary/vidmot/GamePickerActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -114,6 +115,7 @@ public class GamePickerActivity extends AppCompatActivity {
         Context context = getApplicationContext();
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context, text, duration);
+        toast.setGravity(Gravity.CENTER|Gravity.CENTER, 0, 0);
         toast.show();
     }
 
