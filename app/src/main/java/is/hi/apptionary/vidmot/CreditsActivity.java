c97782be562be231986ae2fa93a5844d8f130fb3
Steina <steina.dogg@gmail.com>
2018-03-29 15:21:27 +0000

    Formatted credit text.

--- app/src/main/java/is/hi/apptionary/vidmot/CreditsActivity.java
+++ app/src/main/java/is/hi/apptionary/vidmot/CreditsActivity.java
@@ -3,8 +3,11 @@ package is.hi.apptionary.vidmot;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.Spanned;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import is.hi.apptionary.R;
 
@@ -18,6 +21,8 @@ public class CreditsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
-
+        Spanned sp = Html.fromHtml( getString(R.string.credits_text));
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(sp);
     }
 }
